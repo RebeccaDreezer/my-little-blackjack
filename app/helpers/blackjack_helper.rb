e3f0c6d91947e29aa59f618b4db6563b1da5dd9f
RebeccaDreezer <rebecca@uken.com>
2014-10-29 23:47:25 -0400

    Finish player index page

--- app/helpers/blackjack_helper.rb
+++ app/helpers/blackjack_helper.rb
@@ -1,5 +1,15 @@
 module BlackjackHelper
 
+  def self.display_state(state)
+    view_state = case state
+    when Blackjack::STATE_ACTIVE then "active"
+    when Blackjack::STATE_PLAYER_WIN then "You won!"
+    when Blackjack::STATE_DEALER_WIN then "Dealer won :("
+    else "unknown :("
+    end
+    view_state
+  end
+
   def self.get_card_classes(card)
     suit = self.get_suit(card.slice(0))
     face = card.slice(1,2).to_i

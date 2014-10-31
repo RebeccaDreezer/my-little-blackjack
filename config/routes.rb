29e5428ef07298a107e3002afd8035beab278ee8
RebeccaDreezer <rebecca@uken.com>
2014-10-30 22:25:22 -0400

    Fix log-in flow

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ MyLittleBlackjack::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "sessions#new"
 
   resources :users do
     member do
@@ -25,10 +25,4 @@ MyLittleBlackjack::Application.routes.draw do
       post 'help'
     end
   end
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end

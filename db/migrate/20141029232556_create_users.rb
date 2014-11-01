5ffec50e1e9c79cc82f2d832a6e76dcd4ed27f23
RebeccaDreezer <rebecca@uken.com>
2014-11-01 12:52:10 -0400

    index on user name

--- db/migrate/20141029232556_create_users.rb
+++ db/migrate/20141029232556_create_users.rb
@@ -7,5 +7,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_salt
       t.timestamps
     end
+
+    add_index :users, :name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,4 +44,6 @@ ActiveRecord::Schema.define(version: 20141030234629) do
     t.datetime "updated_at"
   end
 
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
+
 end
